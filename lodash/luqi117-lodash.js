6d84a4865e0bfe70d788e6dcd06c084ef359a297
luqi <18846780941@163.com>
2022-09-14 20:47:48 +0800

    js

---
+++ lodash/luqi117-lodash.js
@@ -0,0 +1,28 @@
+var luqi117 = {
+    chunk:function  (array, number) {
+        var num = []
+        for (var i = 0; i < array.length;) {
+            var ary = []
+            for (var j = 0; j < number; j++, i++) {
+                if (array[i] === undefined) {
+                    break
+                } else {
+
+                    ary.push(array[i])
+                }
+            }
+            num.push(ary)
+        }
+        return num
+    },
+    compact: function compact(array) {
+        var num = []
+        for (var i = 0; i < array.length; i++) {
+
+            if (Boolean(array[i])) {
+                num.push(array[i])
+            }
+        }
+        return num
+    }
+}
